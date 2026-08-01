b57da864fd6ac11d73ea2ee3332b99fec975d285
Em-Keejp <267716525+Em-Keejp@users.noreply.github.com>
2026-08-01 13:20:31 +0900

    update : ショートムービーを1話分追加

--- -Links/ショートムービー/index.js
+++ -Links/ショートムービー/index.js
@@ -1,5 +1,5 @@
 const li_links_template=[
-    createLinks("tale","[ShortMovie] カンニング猫を成敗",""),
+    createLinks("tale","[ShortMovie] カンニング猫を成敗","-Tale/[ShortMovie] カンニング猫を成敗/index.html"),
     createLinks("tale","[ShortMovie] 猫田の日常",""),
     createLinks("tale","[ShortMovie] マラソン大会",""),
     createLinks("tale","[ShortMovie] 町探検",""),
